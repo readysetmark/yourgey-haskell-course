11ce5699992e211cf07d6b8d58af1e641617f09a
Mark Williams <mark@readysetmark.com>
2014-05-17 16:00:24 -0500

    Finished week 2 exercises.

--- week2/LogAnalysis.hs
+++ week2/LogAnalysis.hs
@@ -17,3 +17,38 @@ parseMessage s = case words s of
 
 parse :: String -> [LogMessage]
 parse s = map parseMessage (lines s)
+
+
+-- Putting the logs in order
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert m Leaf 		= Node Leaf m Leaf
+insert m@(LogMessage _ ts _) (Node l n@(LogMessage _ nts _) r)
+	| ts < nts		= Node (insert m l) n r
+	| otherwise		= Node l n (insert m r)
+insert _ t 			= t
+
+
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build (x:xs) = insert x (build xs)
+
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf		 = []
+inOrder (Node l m r) = (inOrder l) ++ [m] ++ (inOrder r)
+
+
+-- Log file postmortem
+
+takeSeverity50plus :: [LogMessage] -> [String]
+takeSeverity50plus [] = []
+takeSeverity50plus ((LogMessage (Error s) _ msg):xs)
+	| s >= 50	= msg : (takeSeverity50plus xs)
+	| otherwise = takeSeverity50plus xs
+takeSeverity50plus (_:xs) = takeSeverity50plus xs
+
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong [] = []
+whatWentWrong m = takeSeverity50plus (inOrder (build m))
